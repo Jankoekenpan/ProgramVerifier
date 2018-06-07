e2fa49e0e1182fae793922a04b052bb61f05ffb8
Jan Boerman <Janboerman95@gmail.com>
2018-06-07 14:04:20 +0200

    initial commit

---
+++ Verifier/src/main/java/ss/group3/programverifier/Main.java
@@ -0,0 +1,11 @@
+package ss.group3.programverifier;
+
+import com.microsoft.z3.Context;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Context context = new Context();
+    }
+
+}
