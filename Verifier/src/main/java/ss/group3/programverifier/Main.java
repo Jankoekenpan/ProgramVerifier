8331d221ad9323742df807b670ee45d5227134b4
Michiel Bakker <michielbakker.1995@gmail.com>
2018-06-07 15:31:19 +0200

    antlr working

--- Verifier/src/main/java/ss/group3/programverifier/Main.java
+++ Verifier/src/main/java/ss/group3/programverifier/Main.java
@@ -1,13 +1,21 @@
 package ss.group3.programverifier;
 
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+
 import com.microsoft.z3.BoolExpr;
 import com.microsoft.z3.Context;
 import com.microsoft.z3.Solver;
 import com.microsoft.z3.Status;
 
+import ss.group3.programverifier.LanguageParser.StatementContext;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Context context = new Context();
 
         //create the solver instance
@@ -27,6 +35,12 @@ public class Main {
         //solver.getModel(), solver.push(), solver.pop(), solver.reset()
         //to read smt files: solver.fromFile(String fileName)
         //to read smt strings: solver.fromString(smtProgram)s
+        
+        CharStream input = new ANTLRInputStream(System.in);
+		LanguageLexer lexer = new LanguageLexer(input);
+		CommonTokenStream stream = new CommonTokenStream(lexer);
+		LanguageParser parser = new LanguageParser(stream);
+		StatementContext statement = parser.statement();
     }
 
 }
