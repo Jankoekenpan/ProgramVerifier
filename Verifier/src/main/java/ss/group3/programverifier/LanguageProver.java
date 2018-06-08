605f967c6385ef0c0d60368b446b5bc4e8830fe7
Jan Boerman <Janboerman95@gmail.com>
2018-06-08 02:58:52 +0200

    now i know where to continue

--- Verifier/src/main/java/ss/group3/programverifier/LanguageProver.java
+++ Verifier/src/main/java/ss/group3/programverifier/LanguageProver.java
@@ -1,5 +1,18 @@
 package ss.group3.programverifier;
 
+import ss.group3.programverifier.ast.Assign;
+import ss.group3.programverifier.ast.Declaration;
+import ss.group3.programverifier.ast.FunctionDef;
+import ss.group3.programverifier.ast.Statement;
+
+import java.util.Collections;
+import java.util.List;
+
 public class LanguageProver extends LanguageBaseListener {
+
+    public List<Statement> toStaticSingleAssignment(FunctionDef functionDefinition) {
+        //TODO
+        return null;
+    }
 
 }
