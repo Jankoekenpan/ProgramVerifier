bc24a3c3fb754745a6e7e3c7bd330a2313c80f1f
Jan Boerman <Janboerman95@gmail.com>
2018-06-08 02:36:50 +0200

    remove unneeded node

--- Verifier/src/main/java/ss/group3/programverifier/ast/While.java
+++ Verifier/src/main/java/ss/group3/programverifier/ast/While.java
@@ -4,8 +4,7 @@ public class While extends Statement {
 
     private final BooleanExpression condition;
     private final Statement body;
-
-
+    
     public While(BooleanExpression condition, Statement body) {
         this.condition = condition;
         this.body = body;
