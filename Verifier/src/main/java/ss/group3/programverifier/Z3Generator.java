1e09de60b62b90f3104d593c36483a89b28696e2
Jan Boerman <Janboerman95@gmail.com>
2018-07-05 16:34:01 +0200

    dont't check, just assert

--- Verifier/src/main/java/ss/group3/programverifier/Z3Generator.java
+++ Verifier/src/main/java/ss/group3/programverifier/Z3Generator.java
@@ -441,7 +441,6 @@ public class Z3Generator extends LanguageBaseVisitor<Void> {
         // we create a new path condition (we are in the loop now)
         ExpressionContext whileCondition = ctx.expression();
         BoolExpr z3wileCondition = (BoolExpr) expr(whileCondition);
-        solver.push(); //everything that happens from here on has this whileCondition
         solver.add(z3wileCondition);
 
         Scope oldScope = curScope();
@@ -501,7 +500,6 @@ public class Z3Generator extends LanguageBaseVisitor<Void> {
         }
 
 
-        solver.push();
         for (String inWhileVar : whileBodyScope.variables.keySet()) {
             //inside the while body, every variabele has a new identifier
             Expr afterWhileVar = newVar(inWhileVar);
@@ -511,14 +509,10 @@ public class Z3Generator extends LanguageBaseVisitor<Void> {
         for (ContractContext contractContext : invariants) {
             ExpressionContext expr = contractContext.expression();
             BoolExpr invariantExpr = (BoolExpr) expr(expr);
-            //TODO is this the correct way to do it?
             BoolExpr terminationExpr = c.mkAnd(invariantExpr, c.mkNot(condition));
-            checkExpression(terminationExpr, ctx, "Couldn't verify ((invariant) && (not condition) after the loop body.");
+            solver.add(terminationExpr);
         }
-        solver.pop();
 
-
-        solver.pop(); //we are no longer in the whileCondition scope
         scopeStack.pop();
 
 	    return null;
